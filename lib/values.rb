b45a36e50da3220c90d0696497de18fbdaad6d05
Justine Bricknell <justinegayton@gmail.com>
2018-06-28 21:45:15 +0000

    automatically backed up by learn

--- lib/values.rb
+++ lib/values.rb
@@ -3,7 +3,7 @@ require 'pry'
 #This is the groceries hash we'll be passing in to the get_the_values method
 # groceries = {
 #  dairy: ["milk", "yogurt", "cheese"],
-#  vegetable: ["carrots", "broccoli", "cucumbers"],
+#  vegetable: ["carrots", rbu"broccoli", "cucumbers"],
 #  meat: ["chicken", "steak", "salmon"],
 #  grains: ["rice", "pasta"]
 # }
